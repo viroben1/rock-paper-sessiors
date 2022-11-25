a2fc5824d170b79bb9b6ae0e42f5c9ea4a2b0897
benjamin farmer <viroben2001@gmail.com>
2022-11-24 16:55:43 -0800

    added js file

---
+++ rock-paper-sessiors.js
@@ -0,0 +1,10 @@
+
+
+
+const chooseUserButton = (button) => {
+
+    // show the button thats been chosen
+    // hide the other button choices
+    let allButton = document.querySelector(".button");
+    allButton.style.display = "none";
+}
