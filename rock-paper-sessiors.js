4c72ae73845d6357e9a4ecf8ffaeb9b0f8724b80
benjamin farmer <viroben2001@gmail.com>
2022-12-05 12:28:02 -0800

    made slight change

--- rock-paper-sessiors.js
+++ rock-paper-sessiors.js
@@ -1,15 +1,13 @@
-window.onload = (event) => {
-const playerScore_span = document.getElementById("#playerScore"); 
-const nonPlayerComputerScore_span = document.getElementById("#nonPlayerComputerScore");
 const rock_div = document.getElementById("rock");
 const sessiors_div = document.getElementById("sessiors");
 const paper_div = document.getElementById("paper");
-}
+
 const  playerScore = 0;
 const nonPlayerComputerScore = 0;
 const scoreBoard_div = document.querySelector("#score-board-container");
-const results_div = document.querySelector("results");
-
+const results_div = document.querySelector("#results");
+const playerScore_span = document.getElementById("playerScore"); 
+const nonPlayerComputerScore_span = document.getElementById("nonPlayerComputerScore");
 
 function getnonPlayerComputerChoice () {
     const choices = ['rock' , 'sessiors', 'paper'];
@@ -18,25 +16,43 @@ function getnonPlayerComputerChoice () {
 }
 getnonPlayerComputerChoice();
 
+function prime () {
+    rock_div.addEventListener('click' , function() {
+        match("rock")
+    });
+    
+    sessiors_div.addEventListener('click' , function() {
+        match("sessiors")
+    });
+    
+    paper_div.addEventListener('click' , function() {
+        match("paper")
+     });
+}
+prime();
+
 function wins (playerChoice, nonPlayerComputerChoice) {
     playerScore++;
     playerScore_span.innerHTML = playerScore;
     nonPlayerComputerScore_span.innerHTML = nonPlayerComputerScore;
-    results_div.innerHTML = (playerChoice + "beats" + nonPlayerComputerChoice + "player won!");
+    results_div.innerHTML = playerChoice + "beats" + nonPlayerComputerChoice + "player won!";
 }
+wins()
 
 function lose (playerChoice, nonPlayerComputerChoice) {
     nonPlayerComputerScore ++;
     playerScore_span.innerHTML = playerScore;
     nonPlayerComputerScore_span.innerHTML = nonPlayerComputerScore;
-    results_div.innerHTML = (nonPlayerComputerChoice + "beats" + playerChoice + "nonPlayerComputer won!");
+    results_div.innerHTML = nonPlayerComputerChoice + "beats" + playerChoice + "nonPlayerComputer won!";
 }
+lose()
 
-function draw (playerChoice, nonPlayerComputerChoice) {
+function draw (playerChoice,  nonPlayerComputerChoice) {
     playerScore_span.innerHTML = playerScore;
     nonPlayerComputerScore_span.innerHTML = nonPlayerComputerScore;
-    results_div.innerHTML = (nonPlayerComputerChoice + "tie game" + playerChoice + "there is a tie! pick again!");
+    results_div.innerHTML = nonPlayerComputerChoice + "tie game" + playerChoice + "there is a tie! pick again!";
 }
+draw()
 
 function match (playerChoice, nonPlayerComputerChoice) {
   switch (playerChoice + nonPlayerComputerChoice) {
@@ -52,26 +68,11 @@ function match (playerChoice, nonPlayerComputerChoice) {
     break;
   case 'rock,rock':
   case 'paper,paper':
-  case 'sessior,ssessiors':
+  case 'sessior,sessiors':
     draw(playerChoice, nonPlayerComputerChoice);
     break;
 };
 }
-
-function prime () {
-    rock_div.addEventListener('click' , function() {
-        match("rock")
-    });
-    
-    sessiors_div.addEventListener('click' , function() {
-        match("sessiors")
-    });
-    
-    paper_div.addEventListener('click' , function() {
-        match("paper")
-     });
-}
-
-prime("rock" , "sessiors", "paper");
+match()
 
 
